576a22dca6d2c67fcb5d99bfac50391a6dc8cd0b
Pav0l <pavol.karas@gmail.com>
2019-05-29 14:25:55 +0200

    Add room relationships

--- src/room.py
+++ src/room.py
@@ -16,6 +16,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    def __str__(self):
-        return f"{self.name} room is {self.description}."
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
