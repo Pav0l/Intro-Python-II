63cee533c241279f1ede579a661b7c6369ab8fe4
Pav0l <pavol.karas@gmail.com>
2019-05-30 13:31:38 +0200

    Add items to room class

--- src/room.py
+++ src/room.py
@@ -1,22 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-"""
-, n_to, s_to, e_to, w_to
-
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
-
-"""
-
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = items
