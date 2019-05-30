ea1a2508a525704eef1e9619ccbb042352fdc55a
Pav0l <pavol.karas@gmail.com>
2019-05-30 13:34:53 +0200

    Remove items from class args

--- src/player.py
+++ src/player.py
@@ -3,7 +3,7 @@
 
 
 class Player:
-    def __init__(self, name, current_room, items):
+    def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        self.items = items
+        self.items = []
