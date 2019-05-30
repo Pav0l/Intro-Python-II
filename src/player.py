5eec1e751ca29f202b9c9110850e4bc344a598a3
Pav0l <pavol.karas@gmail.com>
2019-05-30 15:33:28 +0200

    Add different slot types for player

--- src/player.py
+++ src/player.py
@@ -6,4 +6,7 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        self.items = []
+        self.items = {
+            'melee': None,
+            'ranged': None
+        }
