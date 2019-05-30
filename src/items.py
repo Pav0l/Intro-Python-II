80ef0f790493a2357bc6f94cbf69f415a596e3f5
Pav0l <pavol.karas@gmail.com>
2019-05-30 13:49:43 +0200

    Add slot prop

--- src/items.py
+++ src/items.py
@@ -1,5 +1,6 @@
 class Item:
-    def __init__(self, name, attribute, attribute_value):
+    def __init__(self, name, attribute, attribute_value, slot):
         self.name = name
         self.attribute = attribute
         self.attribute_value = attribute_value
+        self.slot = slot
